86734ff4e8c661e65d4cbca326b174c6462a3100
Abhishek Mukherjee <abhishek.mukher.g@gmail.com>
2012-01-24 11:21:55 -0500

    Update scripts/remove_old_plugins.py

--- scripts/remove_old_plugins.py
+++ scripts/remove_old_plugins.py
@@ -10,22 +10,23 @@ if len(sys.argv) > 1 :
         DRY_RUN = True
 
 vimrc_bundles = get_output("""cat vimrc | grep Bundle | sed '/^"/d' | \
-                awk '{print $2}' | sed "s/'//g" """).split('\n')
+                awk '{print $2}' | sed "s/'//g" """).lower().split('\n')
 
-dir_bundles = get_output("""find bundle/ -maxdepth 1 -type d | sed "s:^bundle/::g" """).split('\n')
+dir_bundles = get_output("""find bundle/ -maxdepth 1 -type d | sed "s:^bundle/::g" """).lower().split('\n')
+
+allowed_bundles = set()
+allowed_bundles.update(EXCEPTIONS)
+allowed_bundles.update(vimrc_bundles)
 
 if DRY_RUN :
     print "On dry run"
 
 for directory in dir_bundles:
-    directory = directory.lower()
-
-    # if the directory (bundle) isn't in vimrc or our exceptions, remove it
-    if not any(directory in bundle.lower() for bundle in vimrc_bundles) \
-            and not any(directory in exception for exception in EXCEPTIONS):
-        command = "rm -rf bundle/%s" % directory
-        print command
-        if not DRY_RUN:
-            get_output(command)
+    if directory in allowed_bundles:
+        continue
+    command = "rm -rf bundle/%s" % directory
+    print command
+    if not DRY_RUN:
+        get_output(command)
 
 
